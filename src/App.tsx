83b9d3718d340433e45137b3a7ea458df2264328
cjdd <459953103@qq.com>
2019-06-04 15:10:32 +0800

    :zap:

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Button>qqq</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
